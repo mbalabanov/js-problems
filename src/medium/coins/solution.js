119e68698d34c272eb3ca692adecc50a4cb65f08
mbalabanov <marincomics@gmail.com>
2022-09-05 16:15:26 +0300

    Tweaks solution for coins (medium) to match expected output

--- src/medium/coins/solution.js
+++ src/medium/coins/solution.js
@@ -1,18 +1,23 @@
-
 function countCoinDenominations(totalMoneyCount) {
     const COIN_DENOMINATIONS = [100, 25, 10, 5, 1]
     let moneyRemaining = totalMoneyCount
-    let coinCounts = ''
+    let coinCounts = `(${totalMoneyCount}) ➞ ` 
 
     for (const COIN of COIN_DENOMINATIONS) {
         if ( COIN <= moneyRemaining) {
             const NUMBER_OF_COINS = Math.floor(moneyRemaining / COIN)
             moneyRemaining = totalMoneyCount % COIN
-            coinCounts += `${NUMBER_OF_COINS} * ${COIN}, `
+            coinCounts += `${NUMBER_OF_COINS} * ${COIN}`
+            if ( COIN > 1) {
+                coinCounts += `, `
+            }
         }
     } 
 
     return coinCounts
 }
 
-console.log(countCoinDenominations(3135))
+console.log(countCoinDenominations(107))
+console.log(countCoinDenominations(57))
+console.log(countCoinDenominations(34))
+console.log(countCoinDenominations(226))
